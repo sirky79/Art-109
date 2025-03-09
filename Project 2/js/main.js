cac8a9502aa2f8b22181b8d17ddc77da2dd1b1ea
sirky79 <shanglongc79@gmail.com>
2025-03-08 23:15:41 -0800

    in progress

---
+++ Project 2/js/main.js
@@ -0,0 +1,28 @@
+
+
+document.querySelector("#next-0").addEventListener("click", function(){
+    document.querySelector("#question-1").style.visibility = "visible";
+    document.querySelector("#question-1").style.opacity = "1";
+});
+
+document.querySelector("#next-1").addEventListener("click", function(){
+    document.querySelector("#question-2").style.visibility = "visible";
+    document.querySelector("#question-2").style.opacity = "1";
+});
+
+document.querySelector("#next-2").addEventListener("click", function(){
+    document.querySelector("#question-3").style.visibility = "visible";
+    document.querySelector("#question-3").style.opacity = "1";
+});
+
+document.querySelector("#next-3").addEventListener("click", function(){
+    document.querySelector("#question-4").style.visibility = "visible";
+    document.querySelector("#question-4").style.opacity = "1";
+});
+
+document.querySelector("#next-4").addEventListener("click", function(){
+    alert("🩸You shouldn't have done that...");
+    document.body.style.backgroundImage = "url('images/freddy.gif')";
+    document.body.style.backgroundPosition = "center";
+    document.body.style.backgroundSize = "cover";
+});
