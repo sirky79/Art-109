10d18d35953aa6e0504c5cde6fe91799c6b5d63b
sirky79 <shanglongc79@gmail.com>
2025-03-09 19:41:40 -0700

    P2 IP

--- Project 2/js/main.js
+++ Project 2/js/main.js
@@ -21,6 +21,26 @@ document.querySelector("#next-3").addEventListener("click", function(){
 });
 
 document.querySelector("#next-4").addEventListener("click", function(){
+    document.querySelector("#question-5").style.visibility = "visible";
+    document.querySelector("#question-5").style.opacity = "1";
+});
+
+document.querySelector("#next-5").addEventListener("click", function(){
+    document.querySelector("#question-6").style.visibility = "visible";
+    document.querySelector("#question-6").style.opacity = "1";
+});
+
+document.querySelector("#next-6").addEventListener("click", function(){
+    document.querySelector("#question-7").style.visibility = "visible";
+    document.querySelector("#question-7").style.opacity = "1";
+});
+
+document.querySelector("#next-7").addEventListener("click", function(){
+    document.querySelector("#question-8").style.visibility = "visible";
+    document.querySelector("#question-8").style.opacity = "1";
+});
+
+document.querySelector("#next-8").addEventListener("click", function(){
     alert("🩸You shouldn't have done that...");
     document.body.style.backgroundImage = "url('images/freddy.gif')";
     document.body.style.backgroundPosition = "center";
