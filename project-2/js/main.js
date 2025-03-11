8ebe29351b286bab463f400c8f7900de8ce8a46c
sirky79 <shanglongc79@gmail.com>
2025-03-10 17:44:05 -0700

    Project 2 Complete

--- project-2/js/main.js
+++ project-2/js/main.js
@@ -2,42 +2,40 @@
 
 document.querySelector("#next-0").addEventListener("click", function(){
     document.querySelector("#question-1").style.visibility = "visible";
-    document.querySelector("#question-1").style.opacity = "1";
 });
 
 document.querySelector("#next-1").addEventListener("click", function(){
     document.querySelector("#question-2").style.visibility = "visible";
-    document.querySelector("#question-2").style.opacity = "1";
 });
 
 document.querySelector("#next-2").addEventListener("click", function(){
     document.querySelector("#question-3").style.visibility = "visible";
-    document.querySelector("#question-3").style.opacity = "1";
+
 });
 
 document.querySelector("#next-3").addEventListener("click", function(){
     document.querySelector("#question-4").style.visibility = "visible";
-    document.querySelector("#question-4").style.opacity = "1";
+
 });
 
 document.querySelector("#next-4").addEventListener("click", function(){
     document.querySelector("#question-5").style.visibility = "visible";
-    document.querySelector("#question-5").style.opacity = "1";
+
 });
 
 document.querySelector("#next-5").addEventListener("click", function(){
     document.querySelector("#question-6").style.visibility = "visible";
-    document.querySelector("#question-6").style.opacity = "1";
+
 });
 
 document.querySelector("#next-6").addEventListener("click", function(){
     document.querySelector("#question-7").style.visibility = "visible";
-    document.querySelector("#question-7").style.opacity = "1";
+
 });
 
 document.querySelector("#next-7").addEventListener("click", function(){
     document.querySelector("#question-8").style.visibility = "visible";
-    document.querySelector("#question-8").style.opacity = "1";
+
 });
 
 document.querySelector("#next-8").addEventListener("click", function(){
