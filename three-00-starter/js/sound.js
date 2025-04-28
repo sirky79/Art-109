1cd4e5334931a63f1c5dc91124702ec4ef6749e8
sirky79 <shanglongc79@gmail.com>
2025-04-27 18:46:19 -0700

    Tut 9

---
+++ three-00-starter/js/sound.js
@@ -0,0 +1,13 @@
+
+
+let song = document.querySelector("#song");
+let playBtn = document.querySelector("#play-button");
+let stopBtn = document.querySelector("#stop-button");
+
+playBtn.addEventListener('click', function () {
+    song.play();
+})
+
+stopBtn.addEventListener('click', function () {
+    song.pause();
+})
